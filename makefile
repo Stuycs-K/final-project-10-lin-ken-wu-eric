bf8b7688081bfd0614808e4b2ca06401f1706cff
Eric Wu <ewu40@stuy.edu>
2024-05-24 15:13:33 -0400

    am able to write to new file in py, need to figure out how to obfuscate specific keywords

---
+++ makefile
@@ -0,0 +1,5 @@
+pyObfuscate: 
+	@python3 obfuscate.py $(ARGS)
+
+clean:
+	@rm -f *.o server client 2> /dev/null || true
--- obfuscate.py
+++ obfuscate.py
@@ -1,9 +1,15 @@
 import sys
+import re
 
-fileName = sys.argv[1];
+fileName = sys.argv[1]
+obfuscatedFileName = "obfuscated" + sys.argv[1]
 
 with open(fileName, "r") as file:
   line = file.readline()
-  while (line)
-    line.strip()
-    line = file.readLine()
+  with open(obfuscatedFileName, "w") as newFile:
+    while (line):
+      line.strip()
+      line = file.readline()
+      # print(line)
+      newLine = re.search("^[A-Z]*$",line)
+      newFile.write(line)
